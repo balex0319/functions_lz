bb4824e98cc88a541fedeec0fb76eca92d4e65b7
balex0319 <borissskk@tutamail.com>
2025-12-01 18:48:56 +0300

    circle.py

--- circle.py
+++ circle.py
@@ -4,17 +4,18 @@ def length(R):
     return 3.14*2*R
 a = length(R)
 a = 3.14*2*R
+print('Длина окружности:',a)
+
 def square(R):
     return 3.14*R**2
 b = square(R)
 b = 3.14*R**2
+print('Площадь окружности:' ,b)
 
 def main():
     R = float(input())
     a = length(R)
     b = square(R)
-    print('Длина окружности:',a)
-    print('Площадь окружности:' ,b)
 if __name__ == '__main__':
-
     main()
+
